605d5089cb8cc250c2148de84abd5ca76ccefc60
NexflixMovies <nexflix.com.movies@gmail.com>
2026-05-21 13:25:38 +0530

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,82 @@
+const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    year: 2010,
+    genre: "Sci-Fi / Thriller",
+    rating: "8.8",
+    description: "A thief who steals corporate secrets through dream-sharing technology is given the task of planting an idea into the mind of a C.E.O.",
+    poster: "https://archive.org/download/inception-2010/inception-2010.jpg",
+    downloadLink: "https://archive.org/download/inception-2010/inception-2010.mp4"
+  },
+  {
+    id: 2,
+    title: "The Dark Knight",
+    year: 2008,
+    genre: "Action / Drama",
+    rating: "9.0",
+    description: "When the Joker wreaks havoc on Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    poster: "https://archive.org/download/the-dark-knight-2008/the-dark-knight.jpg",
+    downloadLink: "https://archive.org/download/the-dark-knight-2008/dark-knight.mp4"
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    year: 2014,
+    genre: "Sci-Fi / Adventure",
+    rating: "8.6",
+    description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    poster: "https://archive.org/download/interstellar-2014/interstellar.jpg",
+    downloadLink: "https://archive.org/download/interstellar-2014/interstellar.mp4"
+  },
+  {
+    id: 4,
+    title: "The Shawshank Redemption",
+    year: 1994,
+    genre: "Drama",
+    rating: "9.3",
+    description: "Two imprisoned men bond over years, finding solace and eventual redemption through acts of common decency.",
+    poster: "https://archive.org/download/shawshank-redemption-1994/shawshank.jpg",
+    downloadLink: "https://archive.org/download/shawshank-redemption-1994/shawshank.mp4"
+  },
+  {
+    id: 5,
+    title: "Pulp Fiction",
+    year: 1994,
+    genre: "Crime / Drama",
+    rating: "8.9",
+    description: "The lives of two mob hitmen, a boxer, a gangster and his wife intertwine in four tales of violence and redemption.",
+    poster: "https://archive.org/download/pulp-fiction-1994/pulp-fiction.jpg",
+    downloadLink: "https://archive.org/download/pulp-fiction-1994/pulp-fiction.mp4"
+  },
+  {
+    id: 6,
+    title: "The Matrix",
+    year: 1999,
+    genre: "Action / Sci-Fi",
+    rating: "8.7",
+    description: "A computer programmer discovers reality is a simulation and joins a rebellion to break free.",
+    poster: "https://archive.org/download/the-matrix-1999/matrix.jpg",
+    downloadLink: "https://archive.org/download/the-matrix-1999/matrix.mp4"
+  },
+  {
+    id: 7,
+    title: "Forrest Gump",
+    year: 1994,
+    genre: "Drama / Romance",
+    rating: "8.8",
+    description: "The presidencies of Kennedy and Johnson unfold through the perspective of an Alabama man with a low IQ but good intentions.",
+    poster: "https://archive.org/download/forrest-gump-1994/forrest-gump.jpg",
+    downloadLink: "https://archive.org/download/forrest-gump-1994/forrest-gump.mp4"
+  },
+  {
+    id: 8,
+    title: "Fight Club",
+    year: 1999,
+    genre: "Drama / Thriller",
+    rating: "8.8",
+    description: "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into something much, much more.",
+    poster: "https://archive.org/download/fight-club-1999/fight-club.jpg",
+    downloadLink: "https://archive.org/download/fight-club-1999/fight-club.mp4"
+  }
+];
